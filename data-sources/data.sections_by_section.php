0586df287f48a10adc67b5600bc6badc1df0ef9a
Stephen Bau <stephen@domain7.com>
2011-10-14 11:26:53 -0700

    Create Sections by Section data source

---
+++ data-sources/data.sections_by_section.php
@@ -0,0 +1,72 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+
+	Class datasourcesections_by_section extends Datasource{
+
+		public $dsParamROOTELEMENT = 'sections-by-section';
+		public $dsParamORDER = 'asc';
+		public $dsParamGROUP = '21';
+		public $dsParamPAGINATERESULTS = 'no';
+		public $dsParamLIMIT = '20';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'sort';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+
+		
+
+		public $dsParamINCLUDEDELEMENTS = array(
+				'title'
+		);
+
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+
+		public function about(){
+			return array(
+				'name' => 'Sections by Section',
+				'author' => array(
+					'name' => 'Stephen Bau',
+					'website' => 'http://home/sym/bootstrap',
+					'email' => 'bauhouse@gmail.com'),
+				'version' => 'Symphony 2.2.3',
+				'release-date' => '2011-09-29T06:13:01+00:00'
+			);
+		}
+
+		public function getSource(){
+			return '1';
+		}
+
+		public function allowEditorToParse(){
+			return true;
+		}
+
+		public function grab(&$param_pool=NULL){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(FrontendPageNotFoundException $e){
+				// Work around. This ensures the 404 page is displayed and
+				// is not picked up by the default catch() statement below
+				FrontendPageNotFoundExceptionHandler::render($e);
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
+			return $result;
+		}
+
+	}
